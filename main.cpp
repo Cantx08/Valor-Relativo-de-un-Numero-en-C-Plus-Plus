115573a86b90e66ff76ec70963e7bfb0407fe15a
Cantx08 <edwin.cantuna@epn.edu.ec>
2024-03-19 23:42:11 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "06 Programa que indica miles, centenas, decenas y unidades de un numero" << endl;
+    int numero, unidadDeMil, centena, decena, unidad;
+
+    // Ingresar el número.
+    // Nota: El número debe ser mayor o igual a 0 y menor a 1000!
+    cout << "Ingrese el numero: ";
+    cin >> numero;
+
+
+    // Calcular el perímetro del círculo.
+    unidadDeMil = numero / 1000;
+    centena = (numero % 1000) / 100;
+    decena = (numero % 100) / 10;
+    unidad = (numero % 10);
+
+
+    // Mostrar el resultado en consola.
+    cout << "El numero es: " << numero << endl;
+    cout << "Unidades de Mil: " << unidadDeMil << "." << endl;
+    cout << "Centenas: " << centena << "." << endl;
+    cout << "Decenas: " << decena << "." << endl;
+    cout << "Unidades: " << unidad << ".";
+    return 0;
+}
